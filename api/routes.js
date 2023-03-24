19db0b772028d86a1da144b33bbc024ff4ec12ae
Julian Calle <julian0986@gmail.com>
2023-03-24 15:59:35 -0500

    Se realiza configuracion de ruta

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,10 @@
 // import products from './products'
 const products = require('./products');
+const users = require('./users');
 
 const routes = (app) => {
   app.use('/api/products', products);
+  app.use('/api/users', users);
 };
 
 module.exports = routes;
