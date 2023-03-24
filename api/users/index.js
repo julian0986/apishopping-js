efc82b1c8bcdbddf43af0eba04f0f7179c4d49fd
Julian Calle <julian0986@gmail.com>
2023-03-24 16:33:57 -0500

    se realiza la configuracion del controler de con el crear y el consultar de users

--- api/users/index.js
+++ api/users/index.js
@@ -5,9 +5,9 @@ const router = new Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.showById);
-router.post('/', controller.create);
-router.delete('/:id', controller.deleteProduct);
-router.put('/:id', controller.updateById);
+router.post('/', controller.createUser);
+// router.delete('/:id', controller.deleteProduct);
+// router.put('/:id', controller.updateById);
 // router.get('/', (req, res) => res.status(200).json({ message: 'hello from products' });
 
 module.exports = router;
---
+++ api/users/users.controller.js
@@ -0,0 +1,37 @@
+const User = require('./user.model');
+
+const respondWithResultUser = (res, code) => (thenResponse) => {
+  if (thenResponse) {
+    res.status(code).json(thenResponse);
+  }
+};
+
+const handleErrorUser = (res, code) => (error) => {
+  if (error) {
+    res.status(code).json(error);
+  }
+};
+
+const index = (req, res) => User.find()
+  .exec()
+  .then(respondWithResultUser(res, 200))
+  .catch(handleErrorUser(res, 500));
+
+const createUser = (req, res) => {
+  if (req.body) {
+    return User.create(req.body)
+      .then(respondWithResultUser(res, 200))
+      .catch(handleErrorUser(res, 500));
+  }
+  return res.status(400).json({ message: 'missing user' });
+};
+
+const showById = (req, res) => User.find({ _id: req.params.id }).exec()
+  .then(respondWithResultUser(res, 200))
+  .catch(handleErrorUser(res, 500));
+
+module.exports = {
+  index,
+  createUser,
+  showById,
+};
